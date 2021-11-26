1d41bd8d0199ed2b8e786b7df949c3742fab16de
grna <kgirius@gmail.com>
2021-11-26 16:35:04 +0200

    styled components with basic styles added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { HeaderWrapper } from "./styledComponents";
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Online Pawnshop</NavLink>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/sell">SELL</NavLink>
-          </li>
-          <li>
-            <NavLink to="/buy">BUY</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <HeaderWrapper>
+      <NavLink to="/">
+        <h4>Online Pawnshop</h4>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to="/">HOME</NavLink>
+        </li>
+        <li>
+          <NavLink to="/sell">SELL</NavLink>
+        </li>
+        <li>
+          <NavLink to="/buy">BUY</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart">CART</NavLink>
+        </li>
+      </ul>
+    </HeaderWrapper>
   );
 };
 
---
+++ src/components/styledComponents.ts
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  background-color: lightgrey;
+  padding: 1rem 2rem;
+
+  & ul {
+    display: flex;
+    justify-content: space-between;
+    width: 30%;
+  }
+`;
+
+export const LandingPageWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+
+  & ul {
+    display: flex;
+    justify-content: space-between;
+    padding: 0;
+    width: 20%;
+  }
+`;
+
+export const ContainerCenter = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const ProductListWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+export const ProductWrapper = styled.div`
+  background-color: lightgrey;
+  width: 31%;
+  margin: 0.5rem;
+  padding: 0.5rem;
+`;
+
+export const ButtonLarge = styled.button`
+  background: ${(props) =>
+    props.style === "danger" ? "tred" : "palevioletred"};
+  border-radius: 3px;
+  border: none;
+  color: white;
+  padding: 0.5rem 1.5rem;
+
+  &:hover {
+    cursor: pointer;
+    border: 2px solid violet;
+  }
+`;
