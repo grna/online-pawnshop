90bfe12077d2ebe69519c809976f465e7bc1586a
grna <kgirius@gmail.com>
2021-11-29 11:44:25 +0200

    reduce product quantity implemented

--- src/redux/actions/productsActions.js
+++ src/redux/actions/productsActions.js
@@ -1,10 +1,34 @@
 import { LOAD_PRODUCTS_SUCCESS } from "../ActionTypes";
 
 export const fetchProducts = () => (dispatch) => {
-  const data = require("../../data.json");
-
-  dispatch({
+  const dispatchData = {
     type: LOAD_PRODUCTS_SUCCESS,
-    payload: data,
-  });
+    payload: [],
+  };
+
+  if (localStorage.getItem("products")) {
+    dispatchData.payload = JSON.parse(localStorage.getItem("products"));
+  } else {
+    dispatchData.payload = require("../../data.json");
+  }
+
+  dispatch(dispatchData);
 };
+
+export const reduceProductQuantity =
+  (cartId, cartQuantity) => (dispatch, getState) => {
+    const products = getState().fromProducts.products.slice();
+    const product = products.find((p) => p._id === cartId);
+    product.quantity -= cartQuantity;
+
+    const dispatchData = {
+      type: LOAD_PRODUCTS_SUCCESS,
+      payload: products,
+    };
+
+    if (product.quantity === 0) {
+      dispatchData.payload = products.filter((p) => p._id !== cartId);
+    }
+    localStorage.setItem("products", JSON.stringify(dispatchData.payload));
+    dispatch(dispatchData);
+  };
